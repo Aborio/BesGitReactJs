cd2a5c69e4146b8693ad9cbe4d446f6d5a618348
Andres Nazzari <lordcliff@gmail.com>
2022-07-01 09:27:55 +0900

    test ajn

--- besappreact/src/components/ItemDetail.js
+++ besappreact/src/components/ItemDetail.js
@@ -1,43 +1,37 @@
 // import productos from "../utils/productos";
-import {Link} from "react-router-dom"
-import { useState, useContext} from "react";
-import {contexto} from "./CartContext"
-import ItemCount from "./ItemCount";
+import { Link } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { contexto } from './CartContext';
+import ItemCount from './ItemCount';
 
+const ItemDetail1 = ({ name, precio, descripcion, image }) => {
+    const context = useContext(contexto);
+    console.log(context);
 
-
-const ItemDetail = ({name,precio,descripcion,image}) => {
-
-    const context = useContext(contexto)
-    console.log(context)
-
-    const [count, setCount] = useState(false)
+    const [count, setCount] = useState(false);
 
     const onAdd = (count) => {
-        setCount(count)
-        alert("se agrego "+ count + " productos")
+        setCount(count);
+        alert('se agrego ' + count + ' productos');
         setCount(true);
-    }
-
+    };
 
     return (
         <div>
-
             <h2>Producto:{name}</h2>
             <h3>Precio: $ {precio}</h3>
-            <img src={image} alt={name} width="400"/>
+            <img src={image} alt={name} width='400' />
             <p>{descripcion}</p>
             {count ? (
                 <>
                     <button onClick={() => context.agregarCarrito()}>confirmar</button>
-                    <Link to="/Carrito">Ir a carrito</Link>
+                    <Link to='/Carrito'>Ir a carrito</Link>
                 </>
-            ):(<ItemCount onAdd={onAdd}/>)}
-            
+            ) : (
+                <ItemCount onAdd={onAdd} />
+            )}
         </div>
-        
-    
-    )
-}
+    );
+};
 
 export default ItemDetail;
